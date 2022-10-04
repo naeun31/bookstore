18d14069da3196d4cd0be0a7ce20305594e6343c
talespin <newtale@naver.com>
2022-10-04 01:52:07 +0000

    env setting

--- server/server.sh
+++ server/server.sh
@@ -1,6 +1,17 @@
+#install python package
 pip install redis flask pymysql gunicorn sqlalchemy
 
+#install nginx
 #sudo apt-get install nginx
 
-
+#start gunicorn flask app
 #nohup gunicorn app:app -b 0.0.0.0:8000 -w 2 --timeout=10 -k gevent >> ../app.log &
+
+#install elasticsearch
+# curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+# echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+# sudo apt update
+# sudo apt install elasticsearch
+# sudo systemctl start elasticsearch
+# sudo systemctl enable elasticsearch
+
