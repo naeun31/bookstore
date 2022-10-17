087f7d73db96205a1ab2f7b6b81f5aca3e6ad017
talespin <newtale@naver.com>
2022-10-17 07:23:59 +0000

    .

--- web/api/admin.py
+++ web/api/admin.py
@@ -3,6 +3,8 @@ import json
 from db import db_session, engine
 from model import *
 from aladin import crawling
+import pandas as pd
+from copy import copy
 
 admin_api = Blueprint('admin', __name__)
 
@@ -12,23 +14,40 @@ def add_books():
     # db_session.add(Book(dct=test))
     # db_session.commit()
 
-    barcodes = params['barcodes']
-    shelf_num = params['shelfNum']
+    barcodes = copy(params['barcodes'])
+    shelf_num = copy(params['shelfNum'])
     result = to_dict(db_session.query(Book).filter(Book.barcode.in_(barcodes)))
- 
+    #book master 등록여부 확인
     if result:
         for i in result:
             #print(i['barcode'])
             barcodes.remove(i['barcode'])
-        
+    #book master 미등록건 등록
     if barcodes:       
         books = crawling(barcodes)
         books.to_sql('book', engine, if_exists='append')
         db_session.commit()
-
         return get_book_list()
     else:
         return 'exist'
+    
+    """
+    #서가등록여부 확인
+    print('서가등록여부확인',shelf_num)
+    result = to_dict(db_session.query(Shelf).filter(Shelf.num == shelf_num))
+    if len(result)==0:
+        shelf = Shelf(dict(num=shelf_num))
+        Shelf.insert(shelf)
+    print('서가등록여부확인끝')
+    #서가 도서등록 시작
+    print('3'*30)
+    print(params['barcodes'])
+    print([dict(barcode=x, num=shelf_num) for x in params['barcodes']])
+    for x in [dict(barcode=x, num=shelf_num) for x in params['barcodes']]:
+        bookshelf = BookShelf(x)
+        BookShelf.insert(bookshelf)
+    #db_session.commit() 
+    """
     
 @admin_api.route('/api/admin/get_book_list', methods=['GET'])
 def get_book_list():
--- web/api/model.py
+++ web/api/model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.orm import relationship
 from db import Base
+from sqlalchemy.dialects.sqlite import insert
 
 class Model():
     def as_dict(self):
@@ -33,3 +34,35 @@ class Book(Base, Model):
         self.publish = dct['publish'] if 'publish' in keys else None
         self.published_date = dct['published_date'] if 'published_date' in keys else None
         self.shelf_num = dct['shelf_num'] if 'shelf_num' in keys else None
+
+class Shelf(Base, Model):
+    __tablename__ = 'shelf'
+    num = Column(Text, primary_key=True)
+
+    def __init__(self, *args, **kwargs):
+        print(1, args, type(args))
+        print(1, kwargs, type(kwargs))
+        self.num = args[0]['num']
+
+    def insert(shelf):
+        stmt = (
+            insert(Shelf).values(num=shelf.num)
+        )
+        print(stmt)
+
+class BookShelf(Base, Model):
+    __tablename__ = 'bookshelf'
+    num = Column(Text, primary_key=True)
+    barcode = Column(Text, primary_key=True)
+
+    def __init__(self, *args, **kwargs):
+        keys = args[0].keys()
+        dct = args[0]
+        self.num = dct['num'] if 'num' in keys else None
+        self.barcode = dct['barcode'] if 'barcode' in keys else None
+
+    def insert(bookshelf):
+        stmt = (
+            insert(BookShelf).values(num=bookshelf.num, barcode=bookshelf.barcode)
+        )
+        print(stmt)
