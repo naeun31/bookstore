f6d13ff376435f4d8856b4a6ed25e7dc1ddb3f86
Choi Jonghwan <newtale@naver.com>
2022-10-25 17:20:06 +0900

    Update elasticsearch_install.sh

--- server/elasticsearch_install.sh
+++ server/elasticsearch_install.sh
@@ -1,15 +1,10 @@
 # https://www.elastic.co/kr/downloads/elasticsearch
-wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.4.2-linux-x86_64.tar.gz"
-tar zxvf elasticsearch-8.4.2-linux-x86_64.tar.gz
-vi elasticsearch-8.4.2/config/elasticsearch.yml
-##xpack.security.enabled: false
-##network.host
-##http.port 수정
+wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.10.2-linux-x86_64.tar.gz"
+tar zxvf elasticsearch-oss-7.10.2-linux-x86_64.tar.gz
+elasticsearch-7.10.2/bin/elasticsearch-plugin install analysis-nori
+#start
+elasticsearch-7.10.2/bin/elasticsearch -d 
+
 #확인
 curl -XGET "http://localhost:9200?pretty"
 
-#한글형태소분석기 설치
-sudo elasticsearch/bin/elasticsearch-plugin install analysis-nori
-
-#background 실행
-elasticsearch/bin/elasticsearch -d
