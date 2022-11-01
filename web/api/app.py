e5dafafb3293e3465ec69ae1929615f737233d3a
Choi Jonghwan <newtale@naver.com>
2022-11-01 17:44:24 +0900

    Update app.py
    book, shelf, bookshelf insert, delete

--- web/api/app.py
+++ web/api/app.py
@@ -83,6 +83,50 @@ def bookshelf():
     sql = """SELECT a.num, b.* FROM bookshelf a, book b WHERE a.barcode = b.barcode AND a.num = :num order by num, published_date DESC, title, publish"""
     return app.config['cursor'].execute(sql, {'num':args['num']).fetchall()
 
+#shelf insert
+def shelf_insert(num):
+    sql =  """INSERT INTO shelf (num) values (:num)"""
+    try:
+        app.config['cursor'].execute(sql, {'num':num})
+        app.config['sqlite'].commit()
+    except:
+        pass
+
+#shelf delete
+def shelf_delete(num):
+    sql =  """SELECT COUNT(*) cnt FROM bookshelf WHERE num = :num"""
+    if app.config['cursor'].execute(sql, {'num':num}).fetchone()[0] <= 0:
+    sql = """DELETE FROM shelf WHERE num = :num"""
+    try:
+        app.config['cursor'].execute(sql, {'num':num})
+        app.config['sqlite'].commit()
+    except:
+        pass
+
+#book insert
+def book_insert(book):
+    try:
+        sql = "INSERT INTO book (" + ' ,'.join(book.keys()) + ") VALUES (:" + ' ,:'.join(book.keys()) + ")"
+        app.config['cursor'].execute(sql, book)
+        return True
+    Except:
+        return False
+
+#bookshelf insert
+def bookshelf_insert(num, barcode):
+    if num == None or num == barcode == None or num == '' or barcode == '': return False
+    sql = """SELECT num FROM shelf WHERE num = :num"""
+    if app.config['cursor'].execute(sql, {'num':num}).fetchone() == None:
+        sql = """INSERT INTO shelf (num) VALUES (:num)"""
+        app.config['cursor'].execute(sql, {'num':num})
+    try:
+        sql = """INSERT INTO bookshelf (num, barcode) VALUES (:num, barcode)"""
+        app.config['cursor'].execute(sql, book)
+        app.config['sqlite'].commit()
+        return True
+    Except:
+        return False
+
 
 if __name__ == '__main__':
     app.run(debug=True)
